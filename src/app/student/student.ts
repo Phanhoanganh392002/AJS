0965fdb4682781102e4c668b8469f7348c575af1
Phanhoanganh392002 <dfang99999@gmail.com>
2022-07-14 11:48:15 +0700

    kienthuc

---
+++ src/app/student/student.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'app-student',
+  //template:'<h1>Day la Component cua Student</h1>'
+  templateUrl:'./student.html'
+})
+export class StudentComponent {
+
+}
